418963b5461ce3eab243b8632245bf1207be2386
Kent Hirv <59705986+kenthirv0@users.noreply.github.com>
2023-11-09 13:17:02 +0200

    Add files via upload

---
+++ ylesanne_5_kent_hirv.ps1
@@ -0,0 +1,22 @@
+﻿# Kent Hirv
+# Tunnikontroll nr 5
+# 09.11.23
+
+# leiab kasutajad
+$users = Get-LocalUser | Where-Object { $_.Enabled -eq $true }
+
+# määrab kuhu csv fail läheb
+$csvPath = "$env:USERPROFILE\Desktop\kasutajad.csv"
+
+# loob csv faili
+Out-File -FilePath $csvPath -Encoding UTF8
+
+
+# iga kasutaja kohta
+foreach ($user in $users) {
+    # lisab mu nime faili
+    Add-Content -Path $csvPath -Value $user
+}
+
+# lisab kommentaari
+Add-Content -Path $csvPath -Value "# Kent Hirv $(Get-Date -Format 'yyyy-MM-dd')"
